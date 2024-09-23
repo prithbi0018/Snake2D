3b7ea68463c269a91108e970cb60390f59d2a3e8
prithbi0018 <prithbirajpanda@gmail.com>
2024-09-23 11:58:36 +0530

    added basic snake sprite bg and created  gamehandle and gameasset script

---
+++ Assets/Scripts/GameAssets.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+
+    public static GameAssets i;
+
+    private void Awake() {
+        i = this;
+    }
+
+    public Sprite snakeHeadSprite;
+
+}
---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+
+public class GameHandler : MonoBehaviour {
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRenderer.sprite = GameAssets.i.snakeHeadSprite;
+    }
+
+}
